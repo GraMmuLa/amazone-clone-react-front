7eb795b7b003a6413956cbb245857b4c074622b4
EeeGorka <usercomfy06558@gmail.com>
2024-04-22 02:59:10 +0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <Header />
+    <>
+      {/* <div className="wrapper"> */}
+      <Header />
+      <main className="page"></main>
+      <Footer />
+      {/* </div> */}
+    </>
   );
 }
 
