171d52a3621ce1984fe13b1384c4eb1c0986327d
GraMmuLa <poliferik@gmail.com>
2024-05-13 12:26:48 +0200

    Fixed some bugs

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -1,17 +1,61 @@
 import styles from "./ProductList.module.css";
-import React from "react";
+import React, {useState} from "react";
 import ProductItem from "./ProductItem";
 import IProductColor from "../../interfaces/IProductColor";
+import ReactPaginate from "react-paginate";
+import paginationClasses from "../pagination/Pagination.module.css";
+import arrow from "../../imgs/arrow.svg";
 
 const ProductList: React.FunctionComponent<{productColors: IProductColor[]}> = ({productColors}) => {
 
+    const itemsPerPage = 3;
+
+    const [itemOffset, setItemOffset] = useState(0);
+
+    const endOffset = itemOffset + itemsPerPage;
+    const currentItems = productColors.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(productColors.length / itemsPerPage);
+
+    const handlePageClick = (event: any) => {
+        const newOffset = (event.selected * itemsPerPage) % productColors.length;
+        setItemOffset(newOffset);
+    };
+
     return (
         <>
-            <div className={styles.productList}>
-                {productColors &&
-                    <div className={styles.productList__items}>
-                        {productColors.map(productColor => <ProductItem key={productColor.id} productColor={productColor}/>)}
-                    </div>
+            <div className={styles.productList__container}>
+                {productColors.length !== 0 &&
+                    <>
+                        <div className={styles.productList}>
+                            <div className={styles.productList__items}>
+                                {currentItems.map(currentItem => <ProductItem key={currentItem.id}
+                                                                              productColor={currentItem}/>)}
+                            </div>
+                            <div className={styles.productList__pagination}>
+                                <ReactPaginate
+                                    containerClassName={paginationClasses.pagination}
+                                    breakLabel="..."
+                                    previousLabel={<img className={paginationClasses.arrowLeftImage} src={arrow}
+                                                        alt="arrow left"/>}
+                                    nextLabel={<img className={paginationClasses.arrowRightImage} src={arrow}
+                                                    alt="arrow right"/>}
+                                    previousClassName={paginationClasses.arrow}
+                                    nextClassName={paginationClasses.arrow}
+                                    disabledClassName={paginationClasses.disabled}
+                                    breakClassName={paginationClasses.disabled}
+                                    onPageChange={handlePageClick}
+                                    pageRangeDisplayed={5}
+                                    pageCount={pageCount}
+                                    pageClassName={paginationClasses.paginationItem}
+                                    activeClassName={paginationClasses.active}
+                                    renderOnZeroPageCount={null}
+                                    marginPagesDisplayed={3}
+                                />
+                            </div>
+                        </div>
+
+
+                    </>
                 }
             </div>
         </>
--- src/components/todaysOffers/body/Body.tsx
+++ src/components/todaysOffers/body/Body.tsx
@@ -1,6 +1,6 @@
 import styles from "./Body.module.css";
 import ProductList from "../../productList/ProductList";
-import React from "react";
+import React, {useState} from "react";
 import IProductColor from "../../../interfaces/IProductColor";
 
 const Body: React.FunctionComponent<{productColors: IProductColor[]}> = ({productColors}) => {
