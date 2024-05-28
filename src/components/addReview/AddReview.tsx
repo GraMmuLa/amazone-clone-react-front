58b2d6bb42b66e26ab4487f5e5f40f28ba997c5d
GraMmuLa <poliferik@gmail.com>
2024-05-28 15:28:30 +0200

    Fixed some bugs

--- src/components/addReview/AddReview.tsx
+++ src/components/addReview/AddReview.tsx
@@ -4,24 +4,28 @@ import StarsRating from "../productPage/starsRating/StarsRating";
 import styles from "./AddReview.module.css";
 import React, {useEffect, useState} from "react";
 import { productColorAPI } from "../../redux/api/productColorAPI";
+import {useAppSelector} from "../../redux/hooks/useAppSelector";
 
 const AddReview: React.FunctionComponent = () => {
    const { productColorId } = useParams<{ productColorId: string }>();
    const { data: productColors } = productColorAPI.useFetchAllQuery({});
 
-   const [quality, setQuality] = useState<string>("");
-   const [priceRatio, setPriceRatio] = useState<string>("");
-   const [deliverySpeed, setDeliverySpeed] = useState<string>("");
+   const {id: userId} = useAppSelector(state => state.user);
+   const [quality, setQuality] = useState<number>(0);
+   const [priceRatio, setPriceRatio] = useState<number>(0);
+   const [deliverySpeed, setDeliverySpeed] = useState<number>(0);
    const [files, setFiles] = useState<File[]>([]);
-
-   let qualityNum = +quality;
-   let priceRatioNum = +priceRatio;
-   let deliverySpeedNum = +deliverySpeed;
+   const [reviewText, setReviewText] = useState<string>();
 
    const addImage = (e: React.ChangeEvent) => {
       const target = (e.target as HTMLInputElement)
       if (target.files != null)
          setFiles([...files, target.files[0]]);
+   }
+
+   //TODO
+   const handleSubmit = (e: React.FormEvent) => {
+      //if(userId)
    }
 
    return (
@@ -38,22 +42,22 @@ const AddReview: React.FunctionComponent = () => {
                <form action="" className={styles.addReview__form}>
                   <div className={styles.addReview__totalReview}>
                      <h2 className={styles.addReview__totalFormTitle}>Загальний відгук</h2>
-                     <StarsRating rating={(qualityNum + priceRatioNum + deliverySpeedNum) / 3} width={38} />
+                     <StarsRating rating={(quality + priceRatio + deliverySpeed) / 3} width={38} />
                   </div>
                   <div className={`${styles.addReview__otherReviews} ${styles.otherReviews}`}>
                      <div className={styles.otherReviews__item}>
                         <h3 className={styles.otherReviews__title}>Якість</h3>
                         <div className={styles.quality__itemsBlock}>
                            <div className={styles.quality__items}>
-                              <input onChange={(e) => setQuality(e.target.value)} className={styles.quality__input} id="quality_5" type="radio" name="qualityRating" value="5" />
+                              <input onChange={(e) => setQuality(parseInt(e.target.value))} className={styles.quality__input} id="quality_5" type="radio" name="qualityRating" value="5" />
                               <label className={styles.quality__label} htmlFor="quality_5"></label>
-                              <input onChange={(e) => setQuality(e.target.value)} className={styles.quality__input} id="quality_4" type="radio" name="qualityRating" value="4" />
+                              <input onChange={(e) => setQuality(parseInt(e.target.value))} className={styles.quality__input} id="quality_4" type="radio" name="qualityRating" value="4" />
                               <label className={styles.quality__label} htmlFor="quality_4"></label>
-                              <input onChange={(e) => setQuality(e.target.value)} className={styles.quality__input} id="quality_3" type="radio" name="qualityRating" value="3" />
+                              <input onChange={(e) => setQuality(parseInt(e.target.value))} className={styles.quality__input} id="quality_3" type="radio" name="qualityRating" value="3" />
                               <label className={styles.quality__label} htmlFor="quality_3"></label>
-                              <input onChange={(e) => setQuality(e.target.value)} className={styles.quality__input} id="quality_2" type="radio" name="qualityRating" value="2" />
+                              <input onChange={(e) => setQuality(parseInt(e.target.value))} className={styles.quality__input} id="quality_2" type="radio" name="qualityRating" value="2" />
                               <label className={styles.quality__label} htmlFor="quality_2"></label>
-                              <input onChange={(e) => setQuality(e.target.value)} className={styles.quality__input} id="quality_1" type="radio" name="qualityRating" value="1" />
+                              <input onChange={(e) => setQuality(parseInt(e.target.value))} className={styles.quality__input} id="quality_1" type="radio" name="qualityRating" value="1" />
                               <label className={styles.quality__label} htmlFor="quality_1"></label>
                            </div>
                         </div>
@@ -62,15 +66,15 @@ const AddReview: React.FunctionComponent = () => {
                         <h3 className={styles.otherReviews__title}>Співвідношення до ціни</h3>
                         <div className={styles.priceRatio__itemsBlock}>
                            <div className={styles.priceRatio__items}>
-                              <input onChange={(e) => setPriceRatio(e.target.value)} className={styles.priceRatio__input} id="priceRatio_5" type="radio" name="priceRatioRating" value="5" />
+                              <input onChange={(e) => setPriceRatio(parseInt(e.target.value))} className={styles.priceRatio__input} id="priceRatio_5" type="radio" name="priceRatioRating" value="5" />
                               <label className={styles.priceRatio__label} htmlFor="priceRatio_5"></label>
-                              <input onChange={(e) => setPriceRatio(e.target.value)} className={styles.priceRatio__input} id="priceRatio_4" type="radio" name="priceRatioRating" value="4" />
+                              <input onChange={(e) => setPriceRatio(parseInt(e.target.value))} className={styles.priceRatio__input} id="priceRatio_4" type="radio" name="priceRatioRating" value="4" />
                               <label className={styles.priceRatio__label} htmlFor="priceRatio_4"></label>
-                              <input onChange={(e) => setPriceRatio(e.target.value)} className={styles.priceRatio__input} id="priceRatio_3" type="radio" name="priceRatioRating" value="3" />
+                              <input onChange={(e) => setPriceRatio(parseInt(e.target.value))} className={styles.priceRatio__input} id="priceRatio_3" type="radio" name="priceRatioRating" value="3" />
                               <label className={styles.priceRatio__label} htmlFor="priceRatio_3"></label>
-                              <input onChange={(e) => setPriceRatio(e.target.value)} className={styles.priceRatio__input} id="priceRatio_2" type="radio" name="priceRatioRating" value="2" />
+                              <input onChange={(e) => setPriceRatio(parseInt(e.target.value))} className={styles.priceRatio__input} id="priceRatio_2" type="radio" name="priceRatioRating" value="2" />
                               <label className={styles.priceRatio__label} htmlFor="priceRatio_2"></label>
-                              <input onChange={(e) => setPriceRatio(e.target.value)} className={styles.priceRatio__input} id="priceRatio_1" type="radio" name="priceRatioRating" value="1" />
+                              <input onChange={(e) => setPriceRatio(parseInt(e.target.value))} className={styles.priceRatio__input} id="priceRatio_1" type="radio" name="priceRatioRating" value="1" />
                               <label className={styles.priceRatio__label} htmlFor="priceRatio_1"></label>
                            </div>
                         </div>
@@ -79,15 +83,15 @@ const AddReview: React.FunctionComponent = () => {
                         <h3 className={styles.otherReviews__title}>Швидкість доставки</h3>
                         <div className={styles.deliverySpeed__itemsBlock}>
                            <div className={styles.deliverySpeed__items}>
-                              <input onChange={(e) => setDeliverySpeed(e.target.value)} className={styles.deliverySpeed__input} id="deliverySpeed_5" type="radio" name="deliverySpeedRating" value="5" />
+                              <input onChange={(e) => setDeliverySpeed(parseInt(e.target.value))} className={styles.deliverySpeed__input} id="deliverySpeed_5" type="radio" name="deliverySpeedRating" value="5" />
                               <label className={styles.deliverySpeed__label} htmlFor="deliverySpeed_5"></label>
-                              <input onChange={(e) => setDeliverySpeed(e.target.value)} className={styles.deliverySpeed__input} id="deliverySpeed_4" type="radio" name="deliverySpeedRating" value="4" />
+                              <input onChange={(e) => setDeliverySpeed(parseInt(e.target.value))} className={styles.deliverySpeed__input} id="deliverySpeed_4" type="radio" name="deliverySpeedRating" value="4" />
                               <label className={styles.deliverySpeed__label} htmlFor="deliverySpeed_4"></label>
-                              <input onChange={(e) => setDeliverySpeed(e.target.value)} className={styles.deliverySpeed__input} id="deliverySpeed_3" type="radio" name="deliverySpeedRating" value="3" />
+                              <input onChange={(e) => setDeliverySpeed(parseInt(e.target.value))} className={styles.deliverySpeed__input} id="deliverySpeed_3" type="radio" name="deliverySpeedRating" value="3" />
                               <label className={styles.deliverySpeed__label} htmlFor="deliverySpeed_3"></label>
-                              <input onChange={(e) => setDeliverySpeed(e.target.value)} className={styles.deliverySpeed__input} id="deliverySpeed_2" type="radio" name="deliverySpeedRating" value="2" />
+                              <input onChange={(e) => setDeliverySpeed(parseInt(e.target.value))} className={styles.deliverySpeed__input} id="deliverySpeed_2" type="radio" name="deliverySpeedRating" value="2" />
                               <label className={styles.deliverySpeed__label} htmlFor="deliverySpeed_2"></label>
-                              <input onChange={(e) => setDeliverySpeed(e.target.value)} className={styles.deliverySpeed__input} id="deliverySpeed_1" type="radio" name="deliverySpeedRating" value="1" />
+                              <input onChange={(e) => setDeliverySpeed(parseInt(e.target.value))} className={styles.deliverySpeed__input} id="deliverySpeed_1" type="radio" name="deliverySpeedRating" value="1" />
                               <label className={styles.deliverySpeed__label} htmlFor="deliverySpeed_1"></label>
                            </div>
                         </div>
@@ -104,7 +108,11 @@ const AddReview: React.FunctionComponent = () => {
                   </div>
                   <div className={styles.addReview__addComment}>
                      <label htmlFor="addReview__addComment">Коментар до відгуку</label>
-                     <div><textarea id="addReview__addComment" placeholder="Що для вас було найцікавішим?" name="addReviewComment"></textarea></div>
+                     <div><textarea value={reviewText ? reviewText : ""}
+                                    id="addReview__addComment"
+                                    placeholder="Що для вас було найцікавішим?"
+                                    name="addReviewComment"
+                                    onChange={(e)=>setReviewText(e.target.value)}></textarea></div>
                   </div>
                   <button type="submit">Надіслати відгук</button>
                </form>
